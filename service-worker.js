5c9b12e7f119b4d39a2cec6fd848cee47d0997fa
datapolitical <datapolitical@gmail.com>
2021-04-08 20:10:27 -0700

    Service worker update

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@ workbox.routing.registerRoute(
 
 // use `CacheFirst` strategy for images
 workbox.routing.registerRoute(
-    /assets\/images/,
+    /assets\/(fontawesome|images)/,
     new workbox.strategies.CacheFirst()
 );
 
