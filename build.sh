999f11dae88fb62c9b9e58c2ffdf650b01ccad69
Chris Nicholson <datapolitical@gmail.com>
2021-11-17 03:13:11 -0700

    Create build script for cloudflare 2

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 pwd
 echo $PATH
+export PATH = "/opt/buildhome/squashfs-root/:$PATH"
 mogrify -version
 cd $HOME
 wget --no-check-certificate "https://download.imagemagick.org/ImageMagick/download/binaries/magick"
 chmod a+x magick
-export MAGICK_HOME="/opt/buildhome"
+export MAGICK_HOME="/opt/buildhome/squashfs-root/"
 ./magick --appimage-extract
 cd /opt/buildhome/repo
 bundle exec jekyll build --destination gh-pages
