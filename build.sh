83a7203a08480b31b1f5316f11d9235588f822fd
Chris Nicholson <datapolitical@gmail.com>
2021-11-17 04:13:47 -0700

    Cloudflare 7

--- build.sh
+++ build.sh
@@ -2,20 +2,14 @@
 
 pwd
 echo $PATH
-export PATH = "/opt/buildhome/squashfs-root/usr/bin:$PATH"
+#!export PATH = "/opt/buildhome/squashfs-root/usr/bin:$PATH"
+apt-get install fuse
 mogrify -version
 cd $HOME
 wget --no-check-certificate "https://download.imagemagick.org/ImageMagick/download/binaries/magick"
 chmod a+x magick
-export MAGICK_HOME="/opt/buildhome/squashfs-root/usr"
-export LD_LIBRARY_PATH="$MAGICK_HOME/lib:$LD_LIBRARY_PATH"
-./magick --appimage-extract
+./magick --version
 echo "zero"
-cd /opt/buildhome
 ls -l
 cd /opt/buildhome/repo
-echo "one"
-/opt/buildhome/squashfs-root/usr/bin/mogrify -version
-echo "two"
-mogrify -version
 bundle exec jekyll build --destination gh-pages
