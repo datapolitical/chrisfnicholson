4ce0a5640f913dd0eedcabe503e6f89307e39ffd
Chris Nicholson <datapolitical@gmail.com>
2021-11-21 09:58:29 -0700

    switching to open weather

--- functions/index.js
+++ functions/index.js
@@ -8,65 +8,28 @@ try{
 
   const { latitude, longitude, city, timezone } = request.cf
 
-    // const { latitude, longitude } = request.cf
+    let endpoint = "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=15e9a7ef375c125ec4d72783818f684b"
     
-  var timeZoneUpdated = "United States/" + timezone.split('/')[1]
-
-  let endpoint = "https://api.weather.gov/points/"
-  const token = "c531734b5df728158946e194a92d2477a713f44d" //Use a token from https://aqicn.org/api/
-
-  // var mylatitude = context.request.cf.latitude
-  // var yourlongitude = context.request.cf.longitude
-  endpoint+= `${latitude},${longitude}`
-  const init = {
+    const init = {
     headers: {
-      "User-Agent" : "datapolitical@gmail.com",
+    //  "User-Agent" : "datapolitical@gmail.com",
 
     },
   }
 
   const responseWeather = await fetch(endpoint,init)
   const content = await responseWeather.json()
-
-  const response2 = await fetch(content.properties.forecastGridData,init)
-  const content2 = await response2.json()
-
-  const nowdate = new Date()
-
-  var isoDateStr = nowdate.toISOString()
-
-  //var propertystring = JSON.stringify(content2.properties.temperature,null,4)
-  var propertystringTest = JSON.stringify(content2.properties.temperature.values,null,4)
-
-  var temparray = content2.properties.temperature.values
-  var dateStr = temparray[6].validTime
-  var tempStr = temparray[6].value
-  var d = new Date(Date.parse(dateStr.split('/')[0]));
-  var humantime = d.toLocaleTimeString('en-US', { timeZone: timezone })
-  var hourStr = d.getHours()
-  var ftemp = "At " + humantime + "the hour is " + hourStr + "and the iso is " + isoDateStr + " the temperature in " + city + " is" + Math.round((1.8 * tempStr) + 32) + "degrees"
-
-  const currd = new Date()
-  currd.setTime(currd.getTime()-(currd.getTime() % 3600000))
-
-  var textIntermediate = currd.toISOString()
-
-  var textSearch = textIntermediate.split('.')[0] + "+00:00/PT1H"
-
-  //Find the object matching the date
-  const obj = temparray.find(o => o.validTime === textSearch)
-
-  const pickedTime = obj.validTime
-
-  var pd = new Date(Date.parse(pickedTime.split('/')[0]));
+    
+    
   var humanPickedTime = pd.toLocaleTimeString('en-US', { timeZone: timezone })
 
   //Get the value from the object
-  const currentTemp = Math.round((1.8 * obj.value) + 32)
+  const currentTemp = content.main.temp
+  const currentTempF = ((9/5)* (currentTemp - 273)) + 32
 
-  const weatherString = "At " + humanPickedTime + " the temperature in " + city + " is " + currentTemp + " degrees"
+  const weatherString = "At " + humanPickedTime + " the temperature in " + city + " is " + currentTempF + " degrees"
   
-  var errorReport = timezone + "\n" + pickedTime + "\n" + textSearch + "\n" + propertystringTest
+  var errorReport = timezone + "\n" + humanPickedTime + "\n" + JSON.stringify(context)
 
   // Find the placeholder in our static asset
   return new HTMLRewriter().on('#weather', {
