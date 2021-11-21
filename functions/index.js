428628e47b08540732700ca688cd61b5166a0fe6
Chris Nicholson <datapolitical@gmail.com>
2021-11-21 01:29:58 -0700

    Okay even closer now

--- functions/index.js
+++ functions/index.js
@@ -44,13 +44,18 @@ try{
   var hourStr = d.getHours()
   var ftemp = "At " + humantime + "the hour is " + hourStr + "and the iso is " + isoDateStr + " the temperature in " + city + " is" + Math.round((1.8 * tempStr) + 32) + "degrees"
 
+  const currd = new Date()
+  currd.setTime(currd.getTime()-(currd.getTime() % 3600000))
 
+  textIntermediate = currd.toISOString()
 
-  // Find the object matching the date
-  //const obj = temparray.find(obj => obj.validTime === '10-10-10')
+  textSearch = textIntermediate.split('Z')[0] + "+00:00/PT1H"
 
-  // Get the value from the object
-  //const value = obj.value;
+  //Find the object matching the date
+  const obj = temparray.find(obj => obj.validTime === textSearch)
+
+  //Get the value from the object
+  const currentTemp = obj.value;
 
 
   // Find the placeholder in our static asset
@@ -58,7 +63,7 @@ try{
     // And act on the element
     element(element) {
       // https://developers.cloudflare.com/workers/runtime-apis/html-rewriter#methods
-      element.setInnerContent(ftemp)
+      element.setInnerContent(currentTemp)
     }
   }).transform(response)
   } catch (thrown){
