74d0df6a1530511084d0aeb4ba3d28a62c6b0fe3
Chris Nicholson <datapolitical@gmail.com>
2021-11-21 18:56:11 -0700

    Okay I think we're good!

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,9 @@ try{
   const { latitude, longitude, city, country, timezone } = request.cf
 
     let endpoint = "https://api.openweathermap.org/data/2.5/weather?"
-    
+
     endpoint += "lat=" + latitude + "&lon=" + longitude + "&appid=15e9a7ef375c125ec4d72783818f684b"
-    
+
     const init = {
     headers: {
     //  "User-Agent" : "datapolitical@gmail.com",
@@ -21,20 +21,20 @@ try{
 
   const responseWeather = await fetch(endpoint,init)
   const content = await responseWeather.json()
-  
+
   const currd = new Date()
-    
-    
+
+
   var humanTime = currd.toLocaleTimeString('en-US', { timeZone: timezone })
 
   //Get the value from the object
   const currentTempC = content.main.temp
-  const weatherIcon = content.weather[0].icon
+  const weatherDescription = content.weather[0].description
   const currentTempF = Math.round(((9/5)* (currentTempC - 273)) + 32)
-  
+
   var currentTempLocal
   var degreesSymbol
-  
+
   switch(country) {
     case "US":
     case "BS":
@@ -51,12 +51,12 @@ try{
       degreesSymbol = "°C"
       break;
   }
-    
-  
+
+
   // US BS KY LR PW FM MH
 
-  const weatherString = "At " + humanTime + " the weather in " + city + "is <img src=\"http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png\">" + " and the temperature is " + currentTempLocal + degreesSymbol + "."
-  
+  const weatherString = "At " + humanTime + " in " + city + "there's " + weatherDescription + " and the temperature is " + currentTempLocal + degreesSymbol + "."
+
   // var errorReport = timezone + "\n" + humanTime + "\n" + JSON.stringify(context)
 
   // Find the placeholder in our static asset
