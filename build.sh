4e984d3077ce09e0424750e3d9bdf59e0f35badb
Chris Nicholson <datapolitical@gmail.com>
2022-01-07 18:57:00 -0800

    And this should handle most of it

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 gem install bundler:2.3.4
 bundle exec jekyll build
 echo "/*" > _headers
-echo -n "Content-Security-Policy-Report-Only: default-src 'none'; " >> _headers
+echo -n "Content-Security-Policy-Report-Only: default-src 'none'; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'self'; worker-src 'self'; font-src 'self';" >> _headers
 echo -n "style-src " >> _headers
 inline_hash=$(echo `cat gh-pages/assets/css/generated-critical.css` | openssl dgst -binary -sha256 | base64)
-echo -n "'sha256-$inline_hash'" >> _headers
-echo -n ";" >> _headers
+echo -n "'sha256-$inline_hash' " >> _headers
+echo -n "'self';" >> _headers
 cp _headers gh-pages/
