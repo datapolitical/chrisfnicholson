9ee842ac8eb260497ea464ca6987fd5c48e501e3
Chris Nicholson <datapolitical@gmail.com>
2022-01-08 01:32:35 -0800

    Add comment to non-working line

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 gem install bundler:2.3.4
 bundle exec jekyll build
 wget https://ajax.cloudflare.com/cdn-cgi/scripts/cloudflare-static/rocket-loader.min.js
-rocket_hash=$(echo `cat rocket-loader.min.js` | openssl dgst -binary -sha256 | base64)
+# rocket_hash=$(echo `cat rocket-loader.min.js` | openssl dgst -binary -sha256 | base64)
 rocket_hash="7S8HHslrpUKGbGUmT/L2MgqW/dfTrlhZaY5GN8XhFqA="
 echo "/*" > _headers
 echo -n "Content-Security-Policy-Report-Only: default-src 'none'; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'self'; worker-src 'self'; script-src 'sha256-$rocket_hash' ajax.cloudflare.com static.cloudflareinsights.com 'self'; font-src 'self';" >> _headers
