424a8b33fffd42c08abd0dd6e0fed6a9154d6693
Chris Nicholson <datapolitical@gmail.com>
2022-09-10 14:04:10 -0700

    One additional try

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 gem update --system
 gem install bundler:2.3.4
-apt-get install html-xml-utils
-apt-get install xxd
+gem install nokogiri
 # mkdir assets/css/
 # touch assets/css/generated-critical.css
 mkdir -p gh-pages/assets/generated
@@ -21,11 +20,13 @@ echo "/*" > _headers
 echo "Report-To: {"group":"default","max_age":31536000,"endpoints":[{"url":"https://chrisfnicholson.report-uri.com/a/d/g"}],"include_subdomains":true}" >> _headers
 echo -n "Content-Security-Policy-Report-Only: default-src 'none'; report-uri https://chrisfnicholson.report-uri.com/r/d/csp/reportOnly; report-to default; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'self'; worker-src 'self'; script-src $google_analytics ajax.cloudflare.com static.cloudflareinsights.com 'self' 'report-sample'; font-src 'self';" >> _headers
 echo -n "style-src " >> _headers
-inline_hash=$(echo `cat gh-pages/index.html | hxselect -i -c style | shasum -a 256 | cut -d' ' -f1 | xxd -r -p | base64`)
-inline_hash_v2=$(echo `cat gh-pages/index.html | hxselect -i -c style | openssl sha256 -binary | openssl base64`)
-echo -n "$inline_hash"
-echo -n "$inline_hash_v2"
-echo -n "'$inline_hash' " >> _headers
+#inline_hash=$(echo `cat gh-pages/index.html | hxselect -i -c style | shasum -a 256 | cut -d' ' -f1 | xxd -r -p | base64`)
+#inline_hash_v2=$(echo `cat gh-pages/index.html | hxselect -i -c style | openssl sha256 -binary | openssl base64`)
+inline_hash_v3=$(echo `cat gh-pages/index.html | nokogiri -e 'puts $_.search('\''style'\'')' | openssl sha256 -binary | openssl base64`)
+#echo -n "$inline_hash"
+#echo -n "$inline_hash_v2"
+echo -n "$inline_hash_v3"
+echo -n "'$inline_hash_v3' " >> _headers
 echo -n "'self' 'report-sample';" >> _headers
 cp _headers gh-pages/
 tar -cvf gh-pages/index_page.tar gh-pages/index.html
