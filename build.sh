5ebeba91c2bdf533e213264af06767c2d861e062
Chris Nicholson <datapolitical@gmail.com>
2022-09-11 03:06:26 -0700

    perhaps this will

--- build.sh
+++ build.sh
@@ -27,8 +27,7 @@ inline_hash_v3=$(echo `cat gh-pages/index.html | nokogiri -e 'puts $_.css("style
 #echo -n "$inline_hash"
 #echo -n "$inline_hash_v2"
 echo -n "hash text is === $inline_hash_v3_text"
-echo -n "hash equals === $ inline_hash_v3"
-#echo -n "'$inline_hash_v3' " >> _headers
-#echo -n "'self' 'report-sample';" >> _headers
-#cp _headers gh-pages/
-#tar -cvf gh-pages/index_page.tar gh-pages/index.html
+echo -n "hash equals === $inline_hash_v3"
+echo -n "'$inline_hash_v3' " >> _headers
+echo -n "'self' 'report-sample';" >> _headers
+cp _headers gh-pages/
