ad806ae3f5b5c4f71f9d364202536d6beb6f48ef
Chris Nicholson <datapolitical@gmail.com>
2023-06-30 22:35:48 -0600

    remove new line

--- criticalCSSparser.py
+++ criticalCSSparser.py
@@ -2,4 +2,4 @@ from bs4 import BeautifulSoup
 
 with open("gh-pages/index.html") as fp:
     soup = BeautifulSoup(fp, "html.parser")
-print(soup.head.style.text)
+print(soup.head.style.text, end='')
