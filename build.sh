0e2dc2faa9ccb04a91da9bbeb840f66f1342701b
Chris Nicholson <datapolitical@gmail.com>
2023-07-14 12:18:25 -0600

    python debug

--- build.sh
+++ build.sh
@@ -20,7 +20,10 @@ echo "/*" > _headers
 echo "Report-To: {"group":"default","max_age":31536000,"endpoints":[{"url":"https://chrisfnicholson.report-uri.com/a/d/g"}],"include_subdomains":true}" >> _headers
 echo -n "Content-Security-Policy-Report-Only: default-src 'none'; report-uri https://chrisfnicholson.report-uri.com/r/d/csp/reportOnly; report-to default; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'self'; worker-src 'self'; script-src $google_analytics ajax.cloudflare.com static.cloudflareinsights.com 'self' 'report-sample'; font-src 'self';" >> _headers
 echo -n "style-src " >> _headers
-inlinehashes gh-pages -o json
+/usr/local/bin/inlinehashes gh-pages -o json
+ls /usr/local/bin
+python3 -m site --user-base
+echo $PATH
 python3 criticalCSSparser.py | openssl sha256 -binary | openssl base64
 inline_hash_v3=$(python3 criticalCSSparser.py | openssl sha256 -binary | openssl base64)
 #echo -n "$inline_hash"
