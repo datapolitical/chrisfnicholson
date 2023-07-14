86c91320c397487ea1e77fbe5904498a4c94d28b
Chris Nicholson <datapolitical@gmail.com>
2023-07-14 10:54:19 -0600

    call In Line hashes directly

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ echo "/*" > _headers
 echo "Report-To: {"group":"default","max_age":31536000,"endpoints":[{"url":"https://chrisfnicholson.report-uri.com/a/d/g"}],"include_subdomains":true}" >> _headers
 echo -n "Content-Security-Policy-Report-Only: default-src 'none'; report-uri https://chrisfnicholson.report-uri.com/r/d/csp/reportOnly; report-to default; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'self'; worker-src 'self'; script-src $google_analytics ajax.cloudflare.com static.cloudflareinsights.com 'self' 'report-sample'; font-src 'self';" >> _headers
 echo -n "style-src " >> _headers
-python3 -m inlinehashes gh-pages -o json
+inlinehashes gh-pages -o json
 python3 criticalCSSparser.py | openssl sha256 -binary | openssl base64
 inline_hash_v3=$(python3 criticalCSSparser.py | openssl sha256 -binary | openssl base64)
 #echo -n "$inline_hash"
