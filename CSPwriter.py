18b882d6a9aaf3e622e9a6806ca84b19f26286e5
Chris Nicholson <datapolitical@gmail.com>
2023-07-15 17:02:10 -0600

    add manifest hash

--- CSPwriter.py
+++ CSPwriter.py
@@ -7,7 +7,7 @@ headers = open("_headers", "w")
 headers.write("/*" + "\n")
 headers.write("Link: </assets/fonts/PublicSans_subset.woff2>; rel=preload" + "\n")
 headers.write("Report-To: {'group':'default','max_age':31536000,'endpoints':[{'url':'https://chrisfnicholson.report-uri.com/a/d/g'}],'include_subdomains':true}" +"\n")
-headers.write("Content-Security-Policy-Report-Only: default-src 'none'; report-uri https://chrisfnicholson.report-uri.com/r/d/csp/reportOnly; report-to default; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'self'; worker-src 'self'; font-src 'self';")
+headers.write("Content-Security-Policy-Report-Only: default-src 'none'; report-uri https://chrisfnicholson.report-uri.com/r/d/csp/reportOnly; report-to default; connect-src 'self' cloudflareinsights.com https://www.google-analytics.com www.google-analytics.com; img-src 'self'; base-uri 'self'; form-action https://chrisfnicholson-staticman.herokuapp.com 'self'; child-src crosshare.org; manifest-src 'sha256-0edVDnyAgE6BNyBQYXswCWA3fk82xEhm5MrAEuxyhYA=' 'self'; worker-src 'self'; font-src 'self';")
 
 
 
